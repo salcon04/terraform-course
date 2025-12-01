ea604e69b688a615c00501cba9ded85ad5c096b7
Silvia Alcon <salcon04.sa@gmail.com>
2025-12-01 02:23:40 -0400

    addinf vnet

--- 09-practica/main.tf
+++ 09-practica/main.tf
@@ -8,6 +8,12 @@ resource "azurerm_resource_group" "rg" {
   name     = "silvia-rg-${terraform.workspace}"
   location = "eastus"
   tags     = var.common_tags
-
 }
 
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.common_tags
+  address_space       = var.address_space
+}
--- 09-practica/variables.tf
+++ 09-practica/variables.tf
@@ -2,3 +2,11 @@ variable "common_tags" {
   description = "common tags"
   type        = map(string)
 }
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "address_space" {
+  type = list(string)
+}
