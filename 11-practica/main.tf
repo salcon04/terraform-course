4b8fb633afac3a8bc1aac2d452afd09aaa940d02
Silvia Alcon <salcon04.sa@gmail.com>
2025-12-01 03:40:27 -0400

    adding lab 11

--- 11-practica/main.tf
+++ 11-practica/main.tf
@@ -1,11 +1,6 @@
-# Workflow
-# Write -> init -> plan -> apply -> destroy
-# terraform apply --auto-aprove, se utiliza para una confirmacion automatica
-# terraform state refresh, refresca los cambios que pudieron haber hecho directamente desde el portal
-# terraform plan -refresh-only, me muestra cambios manuales sin integrarlos al estado
 
 resource "azurerm_resource_group" "rg" {
-  name     = "silvia-rg-${terraform.workspace}"
+  name     = "silvia-rg"
   location = "eastus"
   tags     = var.common_tags
 }
---
+++ 11-practica/version.tf
@@ -0,0 +1,42 @@
+terraform {
+  backend "remote" {
+    hostname         = "app.terraform.io"
+    organization = "courses-terraform-20251201"
+    workspaces {
+      name = "dev"
+    }
+  }
+
+
+  #required_version = ">= 1.13.5"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.54.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.6.1"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.1.0"
+    }
+  }
+}
+
+
+
+# provider "azurerm" {
+#   features {
+#   }
+# }
